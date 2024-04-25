f3ff7d336ea1ab8d6629723335e20aa63221bd6c
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-04-25 11:50:13 +0530

    Update botstatus_filmnest.py

--- botstatus_filmnest.py
+++ botstatus_filmnest.py
@@ -19,35 +19,38 @@ BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split('
 
 async def main_filmnest():
     async with app:
-            while True:
-                print("Checking...")
-                xxx_filmnest = f"〠 | **ʀᴇᴀʟ-ᴛɪᴍᴇ ʙᴏᴛ ꜱᴛᴀᴛᴜꜱ**"
-                for bot in BOT_LIST:
-                    try:
+        while True:
+            print("Checking...")
+            xxx_filmnest = f"〠 | **ʀᴇᴀʟ-ᴛɪᴍᴇ ʙᴏᴛ ꜱᴛᴀᴛᴜꜱ**"
+            for bot in BOT_LIST:
+                try:
+                    dead_count = 0
+                    for _ in range(3):  # Check bot status 3 times
                         yyy_filmnest = await app.send_message(bot, "/start")
-                        aaa = yyy_filmnest.id
                         await asyncio.sleep(20)
                         zzz_filmnest = app.get_chat_history(bot, limit=1)
                         async for ccc in zzz_filmnest:
-                            bbb = ccc.id
-                        if aaa == bbb:
-                            xxx_filmnest += f""\n\n유  @{bot}\n        └ **ᴅᴇᴀᴅ** ❌""
-                            for bot_admin_id in BOT_ADMIN_IDS:
-                                try:
-                                    await app.send_message(int(bot_admin_id), f"ꆛ **ʙᴇᴇᴘ! ʙᴇᴇᴘ!! @{bot} ɪꜱ ᴅᴏᴡɴ** ❌")
-                                except Exception:
-                                    pass
-                            await app.read_chat_history(bot)
-                        else:
-                            xxx_filmnest += f"\n\n유  @{bot}\n        └ **ᴡᴏʀᴋɪɴɢ** ✅"
-                            await app.read_chat_history(bot)
-                    except FloodWait as e:
-                        await asyncio.sleep(e.x)            
-                time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
-                last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-                xxx_filmnest += f"\n\n✔ ᴄʜᴇᴄᴋᴇᴅ ᴏɴ: {last_update} ({TIME_ZONE})\n\n<i>✇ ʀᴇꜰʀᴇꜱʜᴇꜱ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ</i>"
-                await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_filmnest)
-                print(f"ᴄʜᴇᴄᴋᴇᴅ ᴏɴ: {last_update}")                
-                await asyncio.sleep(3600)
-                        
+                            if ccc.id == yyy_filmnest.id:
+                                dead_count += 1
+                                break
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    continue
+
+                if dead_count == 3:  # If bot didn't respond for 3 times, mark as dead
+                    xxx_filmnest += f"\n\n유  @{bot}\n        └ **ᴅᴇᴀᴅ** ❌"
+                    for bot_admin_id in BOT_ADMIN_IDS:
+                        try:
+                            await app.send_message(int(bot_admin_id), f"ꆛ **ʙᴇᴇᴘ! ʙᴇᴇᴘ!! @{bot} ɪꜱ ᴅᴏᴡɴ** ❌")
+                        except Exception:
+                            pass
+                else:
+                    xxx_filmnest += f"\n\n유  @{bot}\n        └ **ᴡᴏʀᴋɪɴɢ** ✅"
+            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+            last_update = time.strftime(f"%d %b %Y at %I:%M %p")
+            xxx_filmnest += f"\n\n✔ ᴄʜᴇᴄᴋᴇᴅ ᴏɴ: {last_update} ({TIME_ZONE})\n\n<i>✇ ʀᴇꜰʀᴇꜱʜᴇꜱ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ</i>"
+            await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_filmnest)
+            print(f"ᴄʜᴇᴄᴋᴇᴅ ᴏɴ: {last_update}")                
+            await asyncio.sleep(3600)
+
 app.run(main_filmnest())
